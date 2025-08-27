6f7a9613ed0188ea2fc84eef320d21f2a1d36de2
piedra69 <mateitotoroquita@gmail.com>
2025-08-26 21:01:14 -0400

    COmmmit final clase 26/08

---
+++ Tareas_Cruds/Tareas_Cruds/Data/TareaDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tareas_Cruds.Models;
+namespace Tareas_Cruds.Data
+{
+    public class TareaDBContext : DbContext
+    {
+        public TareaDBContext(DbContextOptions<TareaDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Tarea> Tareas {  get; set; }
+        protected TareaDBContext() 
+        { 
+        }
+
+
+    } 
+}
+
