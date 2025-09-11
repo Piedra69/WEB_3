71c4571add59dad5917dbb00590c3742899019c0
piedra69 <mateitoroquita@gmail.com>
2025-09-11 00:27:58 -0400

    Commit tarea se realiza la impementacion de las validaciones y menu lateral desplegable para visualizar cada seccion

--- WAMVC/Migrations/20250911034755_2da.Designer.cs
+++ WAMVC/Migrations/20250911034755_2da.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WAMVC.Data;
 
@@ -11,9 +12,11 @@ using WAMVC.Data;
 namespace WAMVC.Migrations
 {
     [DbContext(typeof(ArtesaniasDBContext))]
-    partial class ArtesaniasDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250911034755_2da")]
+    partial class _2da
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
