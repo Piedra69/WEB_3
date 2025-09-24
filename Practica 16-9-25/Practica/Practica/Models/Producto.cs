278dd83b7079a798ea0e71d9a8e2c0cd2207e448
piedra69 <mateitoroquita@gmail.com>
2025-09-24 19:24:29 -0400

    vlidcaciones del modelo producto

--- Practica 16-9-25/Practica/Practica/Models/Producto.cs
+++ Practica 16-9-25/Practica/Practica/Models/Producto.cs
@@ -1,12 +1,23 @@
-﻿namespace Practica.Models
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Practica.Models
 {
     public class Producto
     {
+        [Key]
         public int ProductoId { get; set; }
+
+        [Required(ErrorMessage = "El nombre del producto es obligatorio.")]
+        [StringLength(100, ErrorMessage = "El nombre no puede superar los 100 caracteres.")]
         public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El precio es obligatorio.")]
+        [Range(0.01, 999999.99, ErrorMessage = "El precio debe ser mayor que 0.")]
+        [DataType(DataType.Currency)]
         public decimal Precio { get; set; }
 
-        public ICollection<DetallePedido> DetallesPedido { get; set; }
+        // Relación con DetallePedido
+        public ICollection<DetallePedido> DetallesPedido { get; set; } = new List<DetallePedido>();
     }
-
 }
