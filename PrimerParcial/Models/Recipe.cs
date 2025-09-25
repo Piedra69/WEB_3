cd1c67d603611cff4f6a306a2d73d16e5740544f
piedra69 <mateitoroquita@gmail.com>
2025-09-25 19:31:15 -0400

    creacion del modelo recipes

---
+++ PrimerParcial/Models/Recipe.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.VisualBasic;
+using PrimerParcial.Models;
+
+namespace PrimerParcial.Models
+{
+    public class Recipes
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int PreparationTimeMinutes { get; set; }
+        public int Servings { get; set; }
+        public string Instructions { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        = DateTime.UtcNow;
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+
+
+    }
+}
+//Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+  //                  PreparationTimeMinutes = table.Column<int>(type: "int", nullable: false),
+  //                  Servings = table.Column<int>(type: "int", nullable: false),
+    //                Instructions = table.Column<string>(type: "nvarchar(max)", nullable: false),
+      //              DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+        //            CategoryId
