5ace28207c611b99df3a2601ee88a6fbd76ab208
piedra69 <mateitoroquita@gmail.com>
2025-10-18 12:54:48 -0400

    Commit practica

---
+++ Controllers/ActividadesController.cs
@@ -0,0 +1,31 @@
+﻿using AspNetCoreGeneratedDocument;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NetIdentity.Controllers
+{
+    
+    public class ActividadesController : Controller
+    {
+        [Authorize(Policy = "SoloFemenino")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //vista->controlador->modelo
+        //modelo->controlador->vista
+        //vista->controlador
+        [Authorize(Policy = "menoresEdad")]
+        public IActionResult Deportes()
+        {
+            return View();
+        }
+
+        [Authorize(Policy = "SoloFemenino")]
+        public IActionResult Tareas()
+        {
+            return View();
+        }
+    }
+
+}
--- WAMVC/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-    public string? ErrorMessage { get; set; }
-    public string? StackTrace { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
