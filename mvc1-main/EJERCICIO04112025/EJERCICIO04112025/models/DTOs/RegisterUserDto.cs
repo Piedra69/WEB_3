cc73755635295447932fba7734e2af028075318c
Piedra69 <mateitoroquita@gmail.com>
2025-11-06 21:06:45 -0400

    mvc main modificacion funcional swager

---
+++ mvc1-main/EJERCICIO04112025/EJERCICIO04112025/models/DTOs/RegisterUserDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EJERCICIO04112025.models.DTOs
+{
+    public class RegisterUserDto
+    {
+        [Required, MaxLength(100)]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+
+    // Esta clase es para actualizar un usuario existente mediante el ID
+    public class UpdateUserDto
+    {
+        public int Id { get; set; }
+
+        [Required, MaxLength(100)]
+        public string UserName { get; set; } = string.Empty;
+
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ mvc1-main/EJERCICIO04112025/EJERCICIO04112025/models/User.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EJERCICIO04112025.models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required, MaxLength(100)]
+        public string UserName { get; set; } = string.Empty;
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+
+        public DateTime FechaNacimiento { get; set; }
+        public int edad;
+    }
+}
